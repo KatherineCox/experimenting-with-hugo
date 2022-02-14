c7e649572536c75c3f8698c64cccf4ea0f87794f
Katherine Cox <katherinecox@jhu.edu>
2022-02-14 15:49:56 -0500

    convert hugoify to function, update rmarkdown yaml

--- R/hugoify.R
+++ R/hugoify.R
@@ -1,39 +1,57 @@
-tutorial = "test_tutorials/Biological_Databases_FlyBase/Biological_Databases_Flybase.Rmd"
-destination = file.path("content", "tutorials")
-
-# assume we've been handed an Rmd file that is a learnr tutorial
 # list_files_with_exts() may be useful
 # xfun::dir_create is a wrapper for dir.create() that first checks if it exists
 # - not sure if this is needed?  sticking with dir.create() for now
 # - may be useful for section directories
+# - lean towards nuking everything and starting clean, so, no worries
+# - but may want it to catch errors with conflicting names
+
+
+# for now, assume source is a learnr .Rmd file.
+# - think about other directory structures later
+#
+# args hugo_root, hugo_section
+# e.g. hugo_root = <repo>, hugo_section = "tutorials"
+# becomes <repo>/content/tutorials
+# be flexible about where "content" goes or whether it's required
 
 
 
-# read in the rmd yaml as an R list
-yaml_list <- rmarkdown::yaml_front_matter(tutorial)
+hugoify <- function(source, hugo_section, hugo_root=".", hugo_name=NULL) {
+  # get source directory path, so we can reference it easily
+  source_dir <- dirname(source)
 
-# get name to use as destination directory and create directory
-source_dir <- dirname(tutorial)
-source_filename <- xfun::sans_ext(basename(tutorial))
+  # get name for directory to create in hugo, if not provided
+  if (is.null(hugo_name)) {
+    hugo_name <- xfun::sans_ext(basename(source))
+  }
 
-# create destination directory
-destination_dir <- file.path(destination, source_filename)
-dir.create(destination_dir)
+  # create output directory
+  output_dir <- file.path(hugo_root, "content", hugo_section, hugo_name)
+  dir.create(output_dir)
 
-# copy image and update yaml to point to new image location
-image_filepath <- file.path(source_dir, yaml_list$image)
-image_filename <- basename(yaml_list$image)
-file.copy(image_filepath, destination_dir)
-yaml_list$image <- image_filename
+  # read in the rmd yaml as an R list
+  yaml_list <- rmarkdown::yaml_front_matter(source)
 
-# create (empty) index.md file
-destination_md <- file.path(destination_dir, "index.md")
-file.create(destination_md)
+  # create (empty) index.md file
+  output_md <- file.path(output_dir, "index.md")
+  file.create(output_md)
 
-# write index.md file
-# - convert R list to yaml string
-# - add the yaml delimiters
-yaml_string <- yaml::as.yaml(yaml_list)
-cat("---\n", yaml_string, "---\n", file=destination_md, sep="")
+  # copy image and update yaml to point to new image location
+  image_filepath <- file.path(source_dir, yaml_list$image)
+  image_filename <- basename(yaml_list$image)
+  file.copy(image_filepath, output_dir)
+  yaml_list$image <- image_filename
 
+  # write index.md file
+  # - convert R list to yaml string
+  # - add the yaml delimiters
+  yaml_string <- yaml::as.yaml(yaml_list)
+  cat("---\n", yaml_string, "---\n", file=output_md, sep="")
+}
 
+# DELETE ME!
+# for temporary testing, formalize into proper tests later
+source = "test_tutorials/Biological_Databases_FlyBase/Biological_Databases_Flybase.Rmd"
+hugo_section = "tutorials"
+
+hugoify(source = source, hugo_section = hugo_section)
