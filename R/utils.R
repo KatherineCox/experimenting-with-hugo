06f0ae31ed2689b0373c9a597d5ad0696fd57644
Katherine Cox <katherinecox@jhu.edu>
2022-02-26 16:00:35 -0500

    first draft of read_md

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,31 @@ write_md <- function( yaml=NULL, content=NULL, filename ) {
   yaml_string <- yaml::as.yaml(yaml)
   writeLines(c("---", yaml_string, "---\n", content), con = filename)
 }
+
+read_md <- function(file) {
+  s <- readLines(file)
+  yaml_delim <- stringr::str_which(s, "---")
+
+  # we expect there to be two yaml delimiters
+  if ( !(length(yaml_delim) == 2) ) {
+    # this is a problem, return an error
+  } else {
+    yaml_start <- yaml_delim[1]
+    yaml_end <- yaml_delim[2]
+
+    # get the yaml (between the yaml delimiters)
+    # include the delimiters, although I don't think it's required for yaml.load
+    yaml_string <- s[yaml_start:yaml_end]
+    yaml_string <- paste(yaml_string, collapse="\n")
+
+    # get the content - should be everything after the second delim
+    content <- s[-(1:yaml_end)]
+    content <- paste(content, collapse="\n")
+  }
+
+  list(
+    yaml = yaml_string,
+    content = content
+  )
+
+}
